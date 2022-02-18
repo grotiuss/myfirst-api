99385b9fd22da40e362681291402de6210963b22
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-19 06:40:35 +0700

    update config.js

--- config/config.js
+++ config/config.js
@@ -3,21 +3,19 @@ require('dotenv').config();
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 module.exports = {
-  // "development": {
-  //   "username": DB_USERNAME,
-  //   "password": DB_PASSWORD,
-  //   "database": DB_DATABASE,
-  //   "host": DB_HOST,
-  //   "dialect": "postgres"
-  // },
   "development": {
-    "username": process.env.PG_USER,
-    "password": process.env.PG_PASSWORD,
-    "database": process.env.PG_DATABASE,
-    "host": process.env.PG_HOST,
-    "port": process.env.PG_PORT,
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_DATABASE,
+    "host": DB_HOST,
     "dialect": "postgres",
-    "operatorsAliases": false
+    "protocol": 'postgres',
+    "dialectOptions": {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
 
   "test": {

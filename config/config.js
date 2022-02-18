4ba774bd775d25dd3545730ce5d502e1a42f5f51
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-19 06:36:42 +0700

    update config.js

--- config/config.js
+++ config/config.js
@@ -11,18 +11,13 @@ module.exports = {
   //   "dialect": "postgres"
   // },
   "development": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_DATABASE,
-    "host": DB_HOST,
+    "username": process.env.PG_USER,
+    "password": process.env.PG_PASSWORD,
+    "database": process.env.PG_DATABASE,
+    "host": process.env.PG_HOST,
+    "port": process.env.PG_PORT,
     "dialect": "postgres",
-    "protocol": 'postgres',
-    "dialectOptions": {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    "operatorsAliases": false
   },
 
   "test": {

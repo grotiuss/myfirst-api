6ac1f4eb049181a7b5a69f4fa3b106866e4a08e7
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-19 06:12:15 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -31,18 +31,22 @@ module.exports = {
     "host": DB_HOST,
     "dialect": "postgres"
   },
+  // "production": {
+  //   "username": DB_USERNAME,
+  //   "password": DB_PASSWORD,
+  //   "database": DB_DATABASE,
+  //   "host": DB_HOST,
+  //   "dialect": "postgres",
+  //   "protocol": 'postgres',
+  //   "dialectOptions": {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false
+  //     }
+  //   }
+  // }
   "production": {
-    "username": DB_USERNAME,
-    "password": DB_PASSWORD,
-    "database": DB_DATABASE,
-    "host": DB_HOST,
+    "use_env_variabele": "postgres://vbfdadcnwylqws:2300de92083794989e457babf723d7a7361137cc069676d04e146fec6a3ec402@ec2-54-157-160-218.compute-1.amazonaws.com:5432/d6hojamm62k60k",
     "dialect": "postgres",
-    "protocol": 'postgres',
-    "dialectOptions": {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
   }
 };
